cdff8c13a0d6598a433d73cc3907d314096784a8
Kyle Mulka <repalviglator@yahoo.com>
2012-11-20 04:14:41 -0500

    implemented simple two step funnel: homepage->done

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import os
 import time
 import json
+import uuid
 import base64
 import urllib2
+from collections import defaultdict
 
 import asyncmongo
 import tornado.ioloop
@@ -19,6 +21,66 @@ db = asyncmongo.Client(
     dbuser='metrics',
     dbpass='password'
 )
+
+session_ids = set()
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render('index.html')
+
+class APILoginHandler(tornado.web.RequestHandler):
+    def post(self):
+        data = json.loads(self.request.body)
+        if data['password'] == 'password':
+            session_id = str(uuid.uuid4())
+            session_ids.add(session_id)
+            self.write(json.dumps({'status': 'success', 'data': {'session_id': session_id}}))
+        else:
+            self.write(json.dumps({'status': 'failure'}))
+
+class APIFunnelDataHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        data = json.loads(self.request.body)
+        if data['session_id'] not in session_ids:
+            self.write(json.dumps({'status': 'failure'}))
+            self.finish()
+            return
+
+        funnel_data = db.events.find(
+            {"$or":[{"url": "/"},{"url": "/done"}]},
+            sort=[("session_id", 1), ("timestamp", 1)],
+            callback=self._on_response
+        )
+    def _on_response(self, response, error):
+        if error:
+            self.write(json.dumps({'status': 'failure'}))
+            self.finish()
+            return
+
+        sessions = defaultdict(list)
+        for item in response:
+            sessions[item["session_id"]].append(item["url"])
+
+        data = {"homepage": 0, "done": 0}
+        for session_id, urls in sessions.iteritems():
+            item_data = {"homepage": False, "done": False}
+            for url in urls:
+                if url == "/":
+                    item_data["homepage"] = True
+                elif item_data["homepage"] and url == "/done":
+                    item_data["done"] = True
+
+            if item_data["homepage"]:
+                data["homepage"] += 1
+            if item_data["done"]:
+                data["done"] += 1
+
+        self.write(json.dumps({'status': 'success', 'data': [
+            {"name": "homepage", "value": data["homepage"]},
+            {"name": "done", "value": data["done"]}
+        ]}))
+        self.finish()
 
 # class MainHandler(tornado.web.RequestHandler):
 #     @tornado.web.asynchronous
@@ -85,10 +147,13 @@ class MixpanelTrackHandler(tornado.web.RequestHandler):
 
 
 application = tornado.web.Application([
-    # (r"/", MainHandler),
+    (r"/", MainHandler),
+    (r"/api/login", APILoginHandler),
+    (r"/api/funnel_data", APIFunnelDataHandler),
     (r"/store_event", StoreEventHandler),
     (r"/track/", MixpanelTrackHandler),
-])
+],
+template_path='templates')
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8888))
